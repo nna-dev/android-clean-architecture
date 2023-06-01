e47592f0cbce14bfb47542ad3b741c88eccb2c5f
nawo <nna.dev.950910@gmail.com>
2023-06-01 23:55:09 +0700

    add some resource

--- presentation/src/main/java/com/nna/architecture/presentation/explorer/CarouselAdapter.kt
+++ presentation/src/main/java/com/nna/architecture/presentation/explorer/CarouselAdapter.kt
@@ -1,9 +1,7 @@
 package com.nna.architecture.presentation.explorer
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.nna.architecture.presentation.R
@@ -11,10 +9,9 @@ import com.nna.architecture.presentation.databinding.ItemCarouselBinding
 
 class CarouselAdapter : RecyclerView.Adapter<CarouselAdapter.CarouselViewHolder>() {
     private val carouselList = mutableListOf<CarouselVO>()
-    private lateinit var binding: ItemCarouselBinding
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CarouselViewHolder {
-        binding = ItemCarouselBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = ItemCarouselBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return CarouselViewHolder(binding)
     }
 
